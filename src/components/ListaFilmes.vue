15ef771cda0662acb91ece31b0bed172c701046c
ProgramadorVictor <victorochaandrade@gmail.com>
2025-05-22 22:27:17 -0300

    registro e listagem de filmes

--- src/components/ListaFilmes.vue
+++ src/components/ListaFilmes.vue
@@ -1,11 +1,26 @@
 <template>
     <div>
         <h3>Lista Filmes</h3>
+        <input type="text" name="" id="" placeholder="Nome do filme" v-model="filme">
+        <button type="button" @click="adicionarFilme">Adicionar</button>
+        <br>
+        <ul>
+            <li v-for="(filme, index) in filmes" :key="index">{{ filme }}</li>
+        </ul>
     </div>
 </template>
 <script>
     export default {
-        
+        name: 'ListaFilmes',
+        data: () => ({
+            filmes: [],
+            filme: ''
+        }),
+        methods: {
+            adicionarFilme(){
+                this.filmes.push(this.filme)
+            }
+        }
     }
 </script>
 <style lang="css">
